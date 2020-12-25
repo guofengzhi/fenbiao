920d2b1874517aa2aea77705c5b8e5ec443cf4f7
guofengzhi <48279052+guofengzhi@users.noreply.github.com>
2020-12-25 15:33:58 +0800

    Add files via upload

---
+++ shardingsphere0.sql
@@ -0,0 +1,18 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 8.0.13 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `t_user0` (
+	`id` bigint (64),
+	`name` varchar (60),
+	`sex` char (3)
+); 
+
+create table `t_user1` (
+	`id` bigint (64),
+	`name` varchar (60),
+	`sex` char (3)
+); 
